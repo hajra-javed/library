d5ea6ce006699c1f27b35ae3ef848c59c28fccfe
hajra-javed <hajra_95@hotmail.com>
2023-05-13 19:20:55 +0500

    delete book functionality

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-let myLibrary = [{title: 'Matilda', author: 'Roald Dahl', pages: 100, read: false}];
+let index = 0;
+
+function Book(title, author, pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.index = index++;
+};
+
+
+let myLibrary = [new Book('Matilda', 'Roald Dahl', 100)];
+// console.log(myLibrary);
 
 const library = document.querySelector('.library');
 
-function Book(author, title, pages){
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    // this.read = read;
-};
-
 function displayBook(b){
+
+    console.log(b);
     const book = document.createElement('div');
     const deleteBook = document.createElement('div');
     const author = document.createElement('div');
@@ -34,6 +40,9 @@ function displayBook(b){
 
     deleteBook.addEventListener('click', () => {
         library.removeChild(book);
+        // console.log(b.index);
+        myLibrary.splice(myLibrary.indexOf(myLibrary.filter(a => a.index === b.index)[0]), 1);
+        console.log(myLibrary);
     })
 
 };
@@ -58,7 +67,10 @@ const cancelBtn = document.querySelector('.cancel');
 
 newBookForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    addBookToLibrary({title: title.value, author:author.value, pages: pages.value});
+    addBookToLibrary(new Book(title.value, author.value, pages.value));
+    title.textContent = '';
+    author.textContent = '';
+    pages.textContent = '';
     formContainer.style.display = 'none';
 })
 
